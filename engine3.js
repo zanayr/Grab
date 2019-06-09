dccfd80b92ce5ec5c2e327512994b234dda06197
Ryan Fickenscher <rsficken@gmail.com>
2019-06-09 15:59:05 -0700

    finished the grab function, moving on the the animation engine next

--- engine3.js
+++ engine3.js
@@ -1,31 +1,95 @@
-/*esling-env browser*/
+/*eslint-env browser*/
 
 (function () {
     "use-strict";
+    function _getID (i) {
+        return ('xxxxxxxx-xxxx-' + i % 10 + 'xxx-yxxx-xxxxxxxxxxxx').replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16 | 0,
+                v = c === 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+    //  _getStyle private function should return a property value from a passed DOM
+    //  object
+    function _getStyle (element, property) {
+        return window.getComputedStyle(element, '')[property];
+    }
+    
     
     //  GRAB
-    window.grab = function (selector) {
-        function create(dom) {
-            var g = {
+    window.grab2 = function (selector) {
+        //  _create should create and return a grab object
+        function _create(dom) {
+            //  Define grab object
+            var grab = {
                 element: dom,
-                id: _getID(),
+                id: _getID(0),
                 values: {
                     left: undefined
                 }
             }
             
-            Object.defineProperties(g, {
+            //  Define grab property getters and setters
+            Object.defineProperties(grab, {
                 left: {
                     get: function () {
                         return this.values.left;
                     },
                     set: function (value) {
                         if (typeof value === 'string') {
-                            
+                            if (value.match(/^-?\d+.?\d*?px$/)) {
+                                value = parseFloat(value, 10);
+                            }
+                        }
+                        if (typeof value === 'number' && !Number.isNaN(value)) {
+                            this.element.style.left = value + 'px';
+                            this.values.left = value;
                         }
                     }
                 }
-            })
+            });
+            //  Animate method
+            grab.animate = function() {
+                //  Do stuff
+            }
+            
+            //  Get default values of DOM object
+            grab.left = _getStyle(grab.element, 'left');
+            
+            //  Return grab object
+            return grab;
         }
-    }
+        
+        //  _grabMany should cycle through an array of DOM objects and call _grab for
+        //  each one
+        function _grabMany (them) {
+            return them.map(function (o) {
+                return _create(o);
+            });
+        }
+        
+        //  _grab should parse a DOM object and return a grab object
+        function _grab (it) {
+            if (typeof it === 'string') {
+                if (it.match(/^<[a-zA-Z]+>$/)) {
+                    return _create(document.createElement(it.slice(1, -1).replace(/\s/g, '')));
+                } else if (it.match(/^#[a-zA-Z]/)) {
+                    return _create(document.getElementById(it.slice(1).replace(/\s/g, '')));
+                } else if (it.match(/^\.[a-zA-Z]/)) {
+                    return _grabMany(document.getElementsByClassName(it.slice(1).replace(/\s/g, '')));
+                } else {
+                    return _create(document.querySelector(it));
+                }
+            } else if (Array.isArray(it)) {
+                return it.map(function (o) {
+                    return _grab(o);
+                });
+            } else {
+                return null;
+            }
+        }
+        
+        // return a grab object
+        return _grab(selector);
+    };
 }());
