a90ffa7d7f6b8b6f080cb7bc1822270b98b0a08a
Ryan Fickenscher <rsficken@gmail.com>
2019-06-12 15:25:28 -0700

    fisished animation engine

--- engine3.js
+++ engine3.js
@@ -22,7 +22,7 @@
             lastFT = 0, // Last frame time in ms
             maxFPS = 60, // Maximum frames per second
             delta = 0,
-            timestemp = 1000 / maxFPS,
+            timestep = 1000 / maxFPS,
             frame = 0, // Frame id from request animation frame,
             FPS = 0,
             FTS = 0, // Frames this second
@@ -30,7 +30,7 @@
             waiting = [],
             updates = [],
             garbage = [],
-            easing = {
+            easings = {
                 linear: function (d) {
                     return d;
                 },
@@ -65,16 +65,203 @@
                 values: {
                     current: a,
                     last: undefined,
-                    original: a,
+                    origin: a,
                     target: b,
                     time: 0,
                     vector: b - a
                 }
             };
-            
             Object.defineProperties(c, {
-                
+                current: {
+                    get: function () {
+                        return this.values.current;
+                    },
+                    set: function (value) {
+                        if (!Number.isNaN(parseFloat(value, 10))) {
+                            this.values.current = value;
+                        }
+                    }
+                },
+                distance: {
+                    get: function () {
+                        return Math.abs(this.values.vector);
+                    }
+                },
+                last: {
+                    get: function () {
+                        return this.values.last;
+                    },
+                    set: function (value) {
+                        if (!Number.isNaN(parseFloat(value, 10))) {
+                            this.values.last = value;
+                        }
+                    }
+                },
+                origin: {
+                    get: function () {
+                        return this.values.origin;
+                    }
+                },
+                target: {
+                    get: function () {
+                        return this.values.target;
+                    }
+                },
+                time: {
+                    get: function () {
+                        return this.values.time;
+                    },
+                    set: function (value) {
+                        if (!Number.isNaN(parseFloat(value, 10))) {
+                            this.values.time = value;
+                        }
+                    }
+                },
+                vector: {
+                    get: function () {
+                        return this.values.vector;
+                    }
+                }
             });
+            return c;
+        }
+        function _index (r) {
+            var index = -1;
+            if (typeof reference === 'string') {
+                updates.forEach(function (update, i) {
+                    if (update.object.id === r) {
+                        index = i;
+                    }
+                });
+            }
+            return index;
+        }
+        function _remove (object) {
+            updates = updates.filter(function (update) {
+                return update.object.id !== object.id;
+            });
+        }
+        function _update () {
+            updates.forEach(function (update) {
+                if (!update.finished) {
+                    Object.keys(update.animations).forEach(function (animation) {
+                        var d = 0,
+                            a = update.animations[animation];
+                        a.time = a.time + timestep;
+                        a.last = a.current;
+                        d = update.easing(a.time / update.duration);
+                        if (a.distance * d >= a.distance) {
+                            a.current = a.target;
+                            update.finished = true;
+                        } else {
+                            a.current = a.vector * d + a.origin;
+                        }
+                    });
+                    if (update.finished) {
+                        garbage.push(update);
+                    }
+                }
+            });
+        }
+        function _render (interpolation) {
+            updates.forEach(function (update) {
+                Object.keys(update.animations).forEach(function (animation) {
+                    var a = update.animations[animation];
+                    if (update.finished) {
+                        update.object[animation] = a.target;
+                    } else {
+                        update.object[animation] = a.last + (a.current - a.last) * interpolation;
+                    }
+                });
+            });
+        }
+        function _dispose () {
+            garbage.forEach(function (update) {
+                if (typeof update.complete === 'function') {
+                    update.complete();
+                }
+                _remove(update.object);
+            });
+            garbage.length = 0;
+        }
+        function _panic () {
+            delta = 0;
+        }
+        function _start () {
+            if (!state) {
+                frame = window.requestAnimationFrame(function (timestamp) {
+                    state = true;
+                    _render(1);
+                    lastFT = timestamp;
+                    lastFPS = timestamp;
+                    FTS = 0;
+                    frame = window.requestAnimationFrame(_loop);
+                });
+            }
+        }
+        function _stop () {
+            state = false;
+            window.cancelAnimationFrame(frame);
+        }
+        function _add (object, values, duration, easing, complete) {
+            var animations = {};
+            if (_index(object.id) > -1) {
+                _remove(object);
+            }
+            Object.keys(values).forEach(function (property) {
+                animations[property] = _animation(object[property], values[property]);
+            });
+            waiting.push({
+                object: object,
+                animations: animations,
+                duration: duration,
+                easing: easings[easing],
+                complete: complete,
+                finished: false
+            });
+            if (!state) {
+                _start();
+            }
+        }
+        function _loop (ts) {
+            var count = 0;
+            if (state) {
+                if (ts < lastFPS + timestep) {
+                    frame = window.requestAnimationFrame(_loop);
+                    return;
+                }
+            }
+            delta = delta + (ts - lastFT);
+            lastFT = ts;
+            if (ts > lastFPS + 1000) {
+                FPS = 0.25 * FTS + 0.75 * FPS;
+                lastFPS = ts;
+                FTS = 0;
+            }
+            FTS = FTS + 1;
+            while (delta >= timestep) {
+                if (waiting.length) {
+                    updates = updates.concat(waiting);
+                    waiting.length = 0;
+                }
+                _update();
+                delta = delta - timestep;
+                count = count + 1;
+                if (count >= 240) {
+                    _panic();
+                    break;
+                }
+            }
+            _render(delta / timestep);
+            _dispose();
+            if (updates.length || waiting.length) {
+                frame = window.requestAnimationFrame(_loop);
+            } else {
+                _stop();
+            }
+        }
+        return {
+            add: _add
         }
     }()); 
     
@@ -88,12 +275,21 @@
                 element: dom,
                 id: _getID(0),
                 values: {
+                    height: undefined,
                     left: undefined
                 }
             }
             
             //  Define grab property getters and setters
             Object.defineProperties(grab, {
+                height: {
+                    get: function () {
+                        return this.values.height;
+                    },
+                    set: function (value) {
+                        
+                    }
+                },
                 left: {
                     get: function () {
                         return this.values.left;
@@ -104,7 +300,7 @@
                                 value = parseFloat(value, 10);
                             }
                         }
-                        if (typeof value === 'number' && !Number.isNaN(value)) {
+                        if (!Number.isNaN(parseFloat(value, 10))) {
                             this.element.style.left = value + 'px';
                             this.values.left = value;
                         }
@@ -112,8 +308,8 @@
                 }
             });
             //  Animate method
-            grab.animate = function() {
-                //  Do stuff
+            grab.animate = function(values, duration, easing, complete) {
+                animation.add(this, values, duration || 1000, easing || 'linear', complete);
             }
             
             //  Get default values of DOM object
