95354b17568226828a565753647a1f145a58cd92
Ryan Fickenscher <rsficken@gmail.com>
2020-07-06 23:44:58 -0700

    implemented GrabElement and the grab function

--- src/grab.module-1.0.0.js
+++ src/grab.module-1.0.0.js
@@ -53,9 +53,8 @@ class GrabCollection {
         return this;
     }
 }
-function getId (z) {
-    z = z ? 0 : z;
-    return ('xxxxxxxx-xxxx-' + z % 10 + 'xxx-yxxx-xxxxxxxxxxxx').replace(/[xy]/g, function(c) {
+function uniqueId () {
+    return ('xxxxxxxx-xxxx-xxxx-yxxx-xxxxxxxxxxxx').replace(/[xy]/g, function(c) {
         var r = Math.random() * 16 | 0,
             v = c === 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
@@ -78,40 +77,32 @@ function isLiteral(value) {
 }
 
 // Event Delegation
-const channels = {events: {}, reducers: {}};
-const grab = {};
-function release(event) {
-    document.removeEventListener(event, channels.reducers[event]);
-    delete channels.events[event];
-    delete channels.reducers[event];
+const events = {channels: {}, reducers: {}};
+function release(channel) {
+    document.removeEventListener(event, events.reducers[channel]);
+    delete evetns.channels[channel];
+    delete evetns.reducers[channel];
 }
-function build() {
-    return e => {
-        const type = e.type;
-        Object.keys(channels.events[type]).forEach(id => {
-            const event = channels.events[type][id];
-            if (event.gaurd(e.target)) return event.callback.apply(event.element, [e]);
-        });
+function delegate(channel) {
+    const reducer = e => {
+        for (let id in events.channels[channel]) {
+            const event = events.channels[channel][id];
+            if (id === e.target.getAttribute('data-grab-id')) return event.fn.apply(event.ref, [e]);
+        }
     };
-}
-function delegate(event) {
-    const reducer = build();
-    channels.events[event] = {};
-    channels.reducers[event] = reducer;
-    document.addEventListener(event, reducer);
-}
-function gaurd(element) {
-    if (/^\./g.test(element.selector)) return target => target.classList.contains(element.selector.slice(1));
-    if (/^\#/g.test(element.selector)) return target => target.id === element.selector.slice(1);
-    return () => false;
+    events.channels[channel] = {};
+    events.reducers[channel] = reducer; // save if so we can remove it ?
+    document.addEventListener(channel, reducer);
 }
 
 
 class GrabElement {
-    constructor(selector) {
-        this.grabId = getId(0);
-        this.selector = selector;
-        this.node = document.createElement('p');
+    constructor(node) {
+        if (node.length) node = node[0];
+        const id = uniqueId();
+        node.setAttribute('data-grab-id', id);
+        this.__grabId = id;
+        this.node = node;
         Object.defineProperties(this, {
             attributeList: {
                 get: () => {
@@ -142,6 +133,9 @@ class GrabElement {
                     return result;
                 }
             },
+            grabId: {
+                get: () => this.__grabId
+            },
             href: {
                 get: () => this.node.href,
                 set: value => this.node.href = value
@@ -158,25 +152,25 @@ class GrabElement {
     }
     // Event Handling
     clear() {
-        Object.keys(channels.events).forEach(event => {
-            if (Object.keys(channels.events[event]).includes(this.grabId)) this.off(event);
+        Object.keys(events.channels).forEach(channel => {
+            if (Object.keys(events.channels[channel]).includes(this.grabId)) this.off(channel);
         });
         return this;
     }
-    off(event) {
-        if (Object.keys(channels.events[event]).includes(this.grabId)) delete channels.events[event][this.grabId];
-        if (!Object.keys(channels.events[event]).length) release(event);
+    off(channel) {
+        if (Object.keys(events.channels[channel]).includes(this.grabId)) delete events.channels[channel][this.grabId];
+        if (!Object.keys(events.channels[channel]).length) release(channel);
         return this;
     }
-    on(event, callback) {
-        if (!channels.events[event]) delegate(event);
-        channels.events[event][this.grabId] = {callback: callback, element: this, gaurd: gaurd(this)};
+    on(channel, callback) {
+        if (!events.channels[channel]) delegate(channel);
+        events.channels[channel][this.grabId] = {fn: callback, ref: this};
         return this;
     }
-    once(event, callback) {
-        this.on(event, e => {
+    once(channel, callback) {
+        this.on(channel, e => {
             callback(e);
-            this.off(event);
+            this.off(channel);
         });
         return this;
     }
@@ -222,125 +216,25 @@ class GrabElement {
         return this;
     }
 }
-/*
-var selected;
-        if (typeof selector === 'string' && selector.length) {
-            //  String "selector"
-            if (/^<[a-z][\da-z]*>$/g.test(selector)) {
-                //  Create new DOM object and pass it to `GrabElement`
-                return new GrabElement(document.createElement(selector.slice(1, -1)));
-            } else if (/^#/g.test(selector)) {
-                //  DOM id string passed
-                selected = document.getElementById(selector.slice(1));
-            } else if (/^\./g.test(selector)) {
-                //  DOM class string passed
-                selected = document.getElementsByClassName(selector.slice(1));
-            } else if (/^[a-z][\da-z]*$/g.test(selector)) {
-                //  DOM tag string passed
-                selected = document.getElementsByTagName(selector);
-            } else if (/(#?\.?[a-z][\da-z]*)/g.test(selector)) {
-                //  DOM selector string passed
-                selected = document.querySelectorAll(selector);
-            } else if (selector.includes(',')) {
-                //  A string of selectors delimited by commas has been passed; iterate
-                //  over its values and recursively pass the them to `grab`
-                selected = selector.split(',').map(function (s) {
-                    return grab(s);
-                });
-            }
-            if (selected && selected.length) {
-                //  An array-like object has been created by `grab`, if the object has
-                //  a single value, return a single `GrabElement`, else return a new
-                //  `GrabCollection` object
-                return selected.length > 1 ? new GrabCollection(selected) : new GrabElement(selected[0]);
-            } else if (selected.nodeType) {
-                // A DOM object has been created by `grab`
-                return new GrabElement(selected);
-            }
-        } else if (selector instanceof GrabElement) {
-            return selector;
-        } else if (selector.nodeType) {
-            //  A DOM object
-            return new GrabElement(selector);
-        } else if (selector instanceof GrabCollection || Array.isArray(selector)) {
-            //  Array like object of "selectors"
-            return new GrabCollection(selector);
-        }
-        return null;
 
 
-        if (args.length == 1) {
-        let model = args[0];
-        if (model instanceof ChromaColor || model instanceof ChromaChannels) {
-            return [model.rgb, model.model];
-        } else if (typeof model == 'string' && model.length) {
-            model = model.replace(/\s|#|0x/gi, '').toLowerCase();
-            if (/^[\da-f]{1,8}$/ig.test(model) && (model.length != 5 && model.length != 7)) {
-                return [fromHexa(model), model];
-            } else if (/^[a-z]+$/ig.test(model) && x11[model]) {
-                return [x11[model].map(value => value / 255).concat(1.0), model];
-            } else if (/^rgb?\(((\d+),?){3}\)$/ig.test(model)) {
-                let values = fromRgba(model.match(/(\d+)/g));
-                if (values) return [values, toRgbString(values, true)];
-            } else if (/^rgba\(((\d+\.?\d*),?){4}\)$/ig.test(model)) {
-                let values = fromRgba(model.match(/(\d+\.?\d*)/g));
-                if (values) return [values, toRgbString(values, true)];
-            } else if (/^hsl\(((-?\d+\.?\d*)%?,?){3}\)$/ig.test(model)) {
-                let values = fromHsla(model.match(/(-?\d+\.?\d*)/g));
-                if (values) return [values, toHslString(values, true)];
-            } else if (/^hsla\(((-?\d+\.?\d*)%?,?){4}\)$/ig.test(model)) {
-                let values = fromHsla(model.match(/(-?\d+\.?\d*)/g));
-                if (values) return [values, toHslString(values, true)];
-            } else if (/^hsv\(((-?\d+\.?\d*)%?,?){3}\)$/ig.test(model)) {
-                let values = fromHsva(model.match(/(-?\d+\.?\d*)/g));
-                if (values) return [values, toHsvString(values, true)];
-            } else if (/^hsva\(((-?\d+\.?\d*)%?,?){4}\)$/ig.test(model)) {
-                let values = fromHsva(model.match(/(-?\d+\.?\d*)/g));
-                if (values) return [values, toHsvString(values, true)];
-            }  else if (/^cmyk\(((\d+\.?\d*)%,?){4}\)$/ig.test(model)) {
-                let values = fromCmyk(model.match(/(\d+\.?\d*)/g));
-                if (values) return [values, toCmykString(values)];
-            } else if (/^((\d+\.?\d*),?){3,4}$/ig.test(model) ) {
-                let values = fromRgba(model.match(/(\d+\.?\d*)/g));
-                if (values) return [values, toRgbString(values, true)];
-            }
-        } else if (typeof model == 'number' && isFinite(model)) {
-            return parse(model.toString(16).padStart(6, '0'));
-        } else if (Array.isArray(model) && (model.length == 3 || model.length == 4)) {
-            for (i in model)
-                if (!isContained(parseFloat(model[i], 10), [0, 1])) return null;
-            if (model.length == 3) return [model.map(value => parseFloat(value, 10)).concat(1.0), model.concat(1.0).toString().replace(/,/g, ', ')]
-            return [model.map(value => parseFloat(value, 10)), model.toString().replace(/,/g, ', ')];
-        }
-    } else if (args.length <= 4) {
-        return parse(Array.from(args));
-    }
-    return null;
-        */
-
-    // NOTE: Currently working on implementing the grab function
-    // We should add selectors for id, class
-    // and create element with <>
-const grab = (...args) => {
+function grab(...args) {
     if (args.length === 1) {
-        let selector = args[0];
-        if (selector instanceof GrabElement || GrabCollection) {
-            return selector;
-        } else if (selector instanceof HTMLElement || HTML) {
-        } else if (typeof selector === 'string' && selector.length) {
-            if (/^#[a-z][\da-z-_]*$/ig.test(selector)) {
-                console.log('id only');
-            } else if (/^\.[a-z][\da-z-_]*$/ig) {
-                console.log('class only');
-            }
-        } else if (Array.isArray(selector) && selector.length) {
-            console.log('collection');
+        if (typeof args[0] === 'string' && args[0].length) {
+            if (/^<[a-z]+>$/ig.test(args[0])) return new GrabElement(document.createElement(args[0].replace(/<|>/g, '')));
+            return new GrabElement(document.querySelectorAll(args[0]));
+        } else if (args[0] instanceof GrabElement || GrabCollection) {
+            return args[0];
+        } else if (args[0] instanceof HTMLElement) {
+            return new GrabElement(args[0]);
+        } else if (args[0] instanceof HTMLCollection) {
+            // return new GrabCollection(Object.keys(args[0]).map(key => new GrabElement(args[0][key])));
         }
-    } else {
-        console.log('collection');
+    } else if (args.length > 1) {
+        // return new GrabCollection(args.map(arg => grab(arg)));
     }
     return null;
-};
+}
 grab.async = executor => new Promise(executor);
 grab.get = (path, options={}) => fetch(path, Object.assign({}, options, {method: 'GET'}));
 grab.post = (path, options={}) => fetch(path, Object.assign({}, options, {method: 'POST'}));
